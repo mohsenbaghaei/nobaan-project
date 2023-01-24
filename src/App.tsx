48e6650f9574cac4c536f98a66a26a8f2cea5d62
mohsenbaghaei <111972399+mohsenbaghaei@users.noreply.github.com>
2023-01-24 20:44:49 +0330

    <feat> create route
    create just routes without any design

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom'
+import Products from './pages/Products';
+import Users from './pages/Users';
+import Verification from './pages/Verification';
+import Header from './components/Header';
+import Fotter from './components/Fotter';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className='app'>
+      <Header />
+      <Routes>
+        <Route path='/products' element={<Products/>} />
+        <Route path='/users' element={<Users/>} />
+        <Route path='verification' element={<Verification/>} />
+      </Routes>
+      <Fotter />
     </div>
   );
 }
