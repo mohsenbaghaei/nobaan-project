2709d00b260961c8f3229a21d179f09929807617
mohsenbaghaei <111972399+mohsenbaghaei@users.noreply.github.com>
2023-01-29 00:51:56 +0330

    <feat> verification handel
    i create verification and user component but it need redux tool kit

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,11 +1,70 @@
-import React from 'react';
+import { useState, useRef, useEffect } from "react";
+import "./Pages.css";
+import { AiFillCheckCircle } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
 
 const Users = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const phoneNumberRef = useRef<HTMLInputElement>(null);
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [validPhoneNumber, setValidPhoneNumber] = useState(false);
+  const [errMsg, setErrmsg] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    phoneNumberRef.current?.focus();
+  }, []);
+  useEffect(() => {
+    setErrmsg("");
+    if (phoneNumber.length === 11) {
+      setValidPhoneNumber(true);
+    } else {
+      setValidPhoneNumber(false);
+    }
+  }, [phoneNumber]);
+
+  const handlePhoneNumber = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (validPhoneNumber) {
+      setPhoneNumber("");
+      setValidPhoneNumber(false);
+      navigate("/verification");
+    } else {
+      setErrmsg("Phone number length must be 11");
+    }
+  };
+  return (
+    <div className="verification">
+        {errMsg ? (
+            <div className="err">
+                {errMsg}
+            </div>
+        ) : ''}
+      <form onSubmit={(event) => handlePhoneNumber(event)}>
+        <label htmlFor="phoneNumber">
+          Enter your Phone Number :
+          {validPhoneNumber ? (
+            <span style={{ marginLeft: "5px", color: "green" }}>
+              <AiFillCheckCircle />
+            </span>
+          ) : null}
+        </label>
+        <br />
+        <input
+          ref={phoneNumberRef}
+          type="tel"
+          id="phoneNumber"
+          maxLength={11}
+          value={phoneNumber}
+          className="phoneNumber"
+          placeholder="09030737899"
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
+        <br />
+        <br />
+        <button type="submit" className="verify">Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default Users;
--- src/pages/Verification.tsx
+++ src/pages/Verification.tsx
@@ -1,11 +1,109 @@
-import React from 'react';
+import { useState, useRef, useEffect } from "react";
+import { useNavigate, Link } from "react-router-dom";
 
 const Verification = () => {
-    return (
-        <div>
-            
+  const [verificationCode1, setVerificationCode1] = useState("");
+  const [verificationCode2, setVerificationCode2] = useState("");
+  const [verificationCode3, setVerificationCode3] = useState("");
+  const [verificationCode4, setVerificationCode4] = useState("");
+  const inputRef1 = useRef<HTMLInputElement>(null);
+  const inputRef2 = useRef<HTMLInputElement>(null);
+  const inputRef3 = useRef<HTMLInputElement>(null);
+  const inputRef4 = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    inputRef1.current?.focus();
+  }, []);
+
+  useEffect(() => {
+    if (verificationCode3) {
+      inputRef4.current?.focus();
+    } else if (verificationCode2) {
+      inputRef3.current?.focus();
+    } else if (verificationCode1) {
+      inputRef2.current?.focus();
+    }
+  }, [verificationCode1, verificationCode2, verificationCode3]);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    navigate("/");
+    console.log("object");
+  };
+
+  const handleClear = () => {
+    setVerificationCode1("");
+    setVerificationCode2("");
+    setVerificationCode3("");
+    setVerificationCode4("");
+    inputRef1.current?.focus();
+  };
+
+  return (
+    <div className="verification">
+      <form onSubmit={(event) => handleSubmit(event)} className="verification">
+        <label htmlFor="code">Enter Your 4 digits verification code</label>
+        <div className="code">
+          <br />
+          <input
+            ref={inputRef1}
+            maxLength={1}
+            id="code"
+            type="tel"
+            className="digit"
+            value={verificationCode1}
+            onChange={(e) => setVerificationCode1(e.target.value)}
+          />
+          <input
+            ref={inputRef2}
+            maxLength={1}
+            type="tel"
+            className="digit"
+            value={verificationCode2}
+            onChange={(e) => setVerificationCode2(e.target.value)}
+          />
+          <input
+            ref={inputRef3}
+            maxLength={1}
+            type="tel"
+            className="digit"
+            value={verificationCode3}
+            onChange={(e) => setVerificationCode3(e.target.value)}
+          />
+          <input
+            ref={inputRef4}
+            maxLength={1}
+            type="tel"
+            className="digit"
+            value={verificationCode4}
+            onChange={(e) => setVerificationCode4(e.target.value)}
+          />
         </div>
-    );
+        <div className="verificationbutton">
+          <button
+            type="submit"
+            className="verify"
+            disabled={
+              !verificationCode1 ||
+              !verificationCode2 ||
+              !verificationCode3 ||
+              !verificationCode4
+                ? true
+                : false
+            }
+          >
+            Verify Code
+          </button>
+        </div>
+      </form>
+      <button className="clear" onClick={handleClear}>
+        Clear
+      </button>
+      <br/>
+      <Link to='/users' className="verificationLink">Correct phone number</Link>
+    </div>
+  );
 };
 
 export default Verification;
