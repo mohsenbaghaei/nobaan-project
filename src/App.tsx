ef4cc7678e3c8c890ba0719e74ce753b7309cc5c
mohsenbaghaei <111972399+mohsenbaghaei@users.noreply.github.com>
2023-01-29 22:41:09 +0330

    <perf> code splitting[
    i add splitt the pages components and create loading page

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
+import { Suspense, lazy } from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import Products from "./pages/Products";
-import Users from "./pages/Users";
-import Verification from "./pages/Verification";
-import Header from "./components/Header";
-import Home from "./pages/Home";
+import Loading from "./components/Loading";
+const Products = lazy(() => import("./pages/Products"));
+const Users = lazy(() => import("./pages/Users"));
+const Verification = lazy(() => import("./pages/Verification"));
+const Header = lazy(() => import("./components/Header"));
+const Home = lazy(() => import("./pages/Home"));
 
 function App() {
   return (
     <div className="container">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/users" element={<Users />} />
-        <Route path="/verification" element={<Verification />} />
-      </Routes>
+      <Suspense fallback={<Loading/>}>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/verification" element={<Verification />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
---
+++ src/components/Loading.tsx
@@ -0,0 +1,12 @@
+import "./Components.css";
+
+const Loading = () => {
+    return (
+        <div className='loading'>
+            <p>Loading Page...</p>
+            <p>Please wait</p>
+        </div>
+    );
+};
+
+export default Loading;
